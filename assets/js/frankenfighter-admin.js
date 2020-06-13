89db5fb35bc17e476e22cbcd7202f1364407fb9c
crabcyborg <mike@crabcyb.org>
2020-06-12 21:28:53 -0300

    remove admin file

--- assets/js/frankenfighter-admin.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function() {
-	jQuery('.jsoneditor').each(function() {
-		const editor = new JSONEditor(this, {});
-		editor.set(JSON.parse(decodeURIComponent(jQuery(this).attr('data-config').replace(/\+/g, ' '))));
-
-		// get json
-		//const updatedJson = editor.get()
-	});
-});
